eb3cdc26ba16376b05e59c10be9d8ae6cbad33eb
Tony <todiros@gmail.com>
2015-09-23 21:27:17 +0300

    Renaming the project folder

---
+++ AdvancedCSharpLab/InsertionSort/InsertionSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+// 5 1 19 12 3 6 10 2
+// 0 -1 0 -1 -1 0 -2 3 -1 -3 5 -1
+
+class InsertionSort
+{
+    static void Main()
+    {
+        List<int> set =
+            Console.ReadLine().Split((char[]) null, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+        //Stopwatch counter = new Stopwatch();
+        //counter.Start();
+
+        int size = set.Count;
+        for (int y = 0; y < size; y++)
+        {
+            for (int i = 1; i < size; i++)
+            {
+                if (set[i - 1] > set[i])
+                {
+                    for (int j = i; j >= 0; j--)
+                    {
+                        if (set[j - 1] >= set[j])
+                        {
+                            int number = set[i];
+                            set.RemoveAt(i);
+                            set.Insert(j - 1, number);
+                            break;
+                        }
+                    }
+                    //InsertElement(set,i); -- a lot slower
+                }
+            }
+        }
+
+        //counter.Stop();
+        Console.WriteLine(string.Join(" ", set));
+        //Console.WriteLine(counter.Elapsed);
+    }
+
+    static void InsertElement(List<int> set, int i)
+    {
+        for (int j = i; j >= 0; j--)
+        {
+            if (set[j - 1] >= set[j])
+            {
+                int number = set[i];
+                set.RemoveAt(i);
+                set.Insert(j - 1, number);
+                break;
+            }
+        }
+    }
+}
---
+++ AdvancedCSharpLab/SieveOfEratosthenes/SieveOfEratosthenes.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class SieveOfEratosthenes
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        n++;
+        
+        List<int> set = Enumerable.Range(-1, n).Select(x => x + 1).ToList();
+        bool[] isPrime = new bool[set.Count];
+
+        for (int i = 2; i < isPrime.Length; i++)
+        {
+            isPrime[i] = true;
+        }
+ 
+        for (int i = 2; i < n; i++)
+        {
+            if (isPrime[i])
+            {
+                for (int y = i * 2; y < n; y = y + i)
+                {
+                    isPrime[y] = false;
+                }
+            }
+        }
+
+        for (int i = 0; i < set.Count; i++)
+        {
+            if (!(isPrime[i]))
+            {
+                set[i] = 0;
+            }
+        }
+
+        set.RemoveAll(x => x == 0);
+        Console.WriteLine(string.Join(", ", set));
+    }
+}
