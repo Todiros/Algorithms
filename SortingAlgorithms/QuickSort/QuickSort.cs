978f577c3afd24762fe5b09182f9d84d456d2eca
Tony <todiros@gmail.com>
2015-10-01 21:02:28 +0300

    Changes to QuickSort from SortingAlgorithms

--- SortingAlgorithms/QuickSort/QuickSort.cs
+++ SortingAlgorithms/QuickSort/QuickSort.cs
@@ -1,15 +1,31 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace QuickSort
+class QuickSort
 {
-    class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int[] arr = new[] {3, 44, 38, 5, 47, 15, 36, 26, 27, 2, 46, 4, 19, 50, 48};
+        Console.WriteLine(string.Join(", ", arr));
+
+        MyQuickSort(arr,0);
+
+        Console.WriteLine(string.Join(", ", arr));
+    }
+
+    private static void MyQuickSort(int[] arr, int p)
+    {
+        int pivot = arr[p];
+
+        for (int i = p; i < arr.Length; i++)
         {
+            if (pivot > arr[i])
+            {
+                pivot ^= arr[i];
+                arr[i] ^= pivot;
+                pivot ^= arr[i];
+
+                arr[p] = pivot;
+            }
         }
     }
 }
